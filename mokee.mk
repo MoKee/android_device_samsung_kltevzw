7260cafdffff89194be32ce7257f3bda0e8a41d2
GaHoKwan <kwangaho@gmail.com>
2016-11-26 20:39:24 +0800

    kltevzw: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: I9addde45415e367498461f48cba7851d8feb0141

--- mokee.mk
+++ mokee.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/kltevzw/full_kltevzw.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
-
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
