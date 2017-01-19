30af3efc096393d922ccfab291eedd1d6b921286
martincz <martincz@mokeedev.com>
2017-01-19 14:52:59 +0800

    kltevzw: mokee-ify

---
+++ mokee.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/kltevzw/full_kltevzw.mk)
+
+PRODUCT_DEVICE := kltevzw
+PRODUCT_NAME := mk_kltevzw
